a5a60deedd1a4a45fa0d617fa80e07bc0e150eb7
googlewaitme <kusok.apelbsina@hotmail.com>
2023-08-17 22:48:31 +0300

    added: class client

---
+++ src/client.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <regex>
+
+
+class Client {
+private:
+    std::string m_name;
+    int m_id;
+public:
+    Client() {
+        m_name = "default_name";
+        m_id = 0;
+    }
+
+    Client(std::string name, int id) {
+        m_name = name;
+        m_id = id;
+    }
+
+    bool is_valid_name() {
+        // TODO error here
+        static const std::regex r("[a-z0-9-_]");
+        return regex_match(m_name, r);
+    }
+};
