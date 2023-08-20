855b9275848dbbda41b54937972322b8ef576cdc
googlewaitme <kusok.apelbsina@hotmail.com>
2023-08-20 21:09:41 +0300

    split event_handling on functions

--- src/computer_club.cpp
+++ src/computer_club.cpp
@@ -3,22 +3,50 @@
 #include <iostream>
 
 #include "computer_club.h"
-#include "event.cpp"
+#include "event.h"
 
 
 ComputerClub::ComputerClub(std::string filename) : file(filename) {
-    file >> m_count_of_tables >> start_time >> end_time >> m_cost_per_hour;
+    file >> m_count_of_tables >> m_start_time >> m_end_time >> m_cost_per_hour;
 }
 
 
 void ComputerClub::event_handling() {
     Event event;
+    std::cout << m_start_time << std::endl;
     while (file >> event) {
         std::cout << event << "\n";
-        if (event.time < start_time || event.time > end_time) {
-            std::cout << event.time << " 13 NotOpenYet\n";
-            continue;
-        }
+        handle(event);
+        
     }
+}
+
+
+void ComputerClub::handle(Event event) {
+    switch(event.type) {
+        case 1: client_come(event); break;
+        case 2: client_sit_at_table(event); break;
+        case 3: client_is_waiting(event); break;
+        case 4: client_left(event); break;
+    }
+}
+
+
+void ComputerClub::client_come (Event event) {
+    if (event.time < m_start_time || event.time > m_end_time) {
+        std::cout << event.time << " 13 NotOpenYet\n";
+    }
+}
+
+void ComputerClub::client_sit_at_table(Event event) {
+
+}
+
+void ComputerClub::client_is_waiting(Event event) {
+
+}
+
+void ComputerClub::client_left(Event event) {
+
 }
 
--- src/computer_club.h
+++ src/computer_club.h
@@ -1,19 +1,29 @@
 #pragma once
+#include <queue>
 #include <string>
 #include <unordered_map>
 #include <fstream>
 
 #include "mytime.h"
+#include "event.h"
 
 
 class ComputerClub {
 private:
     int m_cost_per_hour;
     int m_count_of_tables;
-    Time start_time, end_time;
+    Time m_start_time, m_end_time;
     std::unordered_map<std::string, int> m_clients;
     std::ifstream file;
+    std::queue<std::string> clients_queue;
 public:
     ComputerClub(std::string filename);
     void event_handling();
+
+    void handle(Event event);
+
+    void client_come (Event event);
+    void client_sit_at_table(Event event);
+    void client_is_waiting(Event event);
+    void client_left(Event event);
 };
